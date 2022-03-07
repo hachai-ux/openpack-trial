654abd2ccbcb3063bd63fa622796c352de133592
Hac Hai Pham <hpham.ux@gmail.com>
2022-03-07 11:19:28 +0100

    Style sidebar

--- openpack-trial/src/components/Sidebar.js
+++ openpack-trial/src/components/Sidebar.js
@@ -6,43 +6,50 @@ const Sidebar = (props) => {
 
 
     return (
-        <nav className="h-screen fixed top-0 width-12">
+        <nav className="h-screen fixed top-0 width-12 border-r-2 pr-20 pt-14 pl-10">
             <ul className="list-none">
                 <li>
-                    <a href='#'></a>
-                    <HomeIcon className="h-5 w-5 text-blue-500" />
-                    <span>Cockpit</span>
+                    <a href='#' className="flex items-center px-2 py-2">
+                    <HomeIcon className="h-5 w-5 text-gray-600" />
+                        <span>Cockpit</span>
+                        </a>
                 </li>
                 <li>
-                    <a href='#'></a>
-                    <OfficeBuildingIcon className="h-5 w-5 text-blue-500" />
-                    <span>Digital Factory</span>
+                    <a href='#' className="flex items-center px-2 py-2">
+                    <OfficeBuildingIcon className="h-5 w-5 text-gray-600" />
+                        <span>Digital Factory</span>
+                        </a>
                 </li>
                 <li>
-                    <a href='#'></a>
-                    <ShoppingCartIcon className="h-5 w-5 text-blue-500" />
-                    <span>Market Place</span>
+                    <a href='#' className="flex items-center px-2 py-2">
+                    <ShoppingCartIcon className="h-5 w-5 text-gray-600" />
+                        <span>Market Place</span>
+                        </a>
                 </li>
                 <li>
-                    <a href='#'></a>
-                    <HandIcon className="h-5 w-5 text-blue-500" />
-                    <span>Service Store</span>
+                    <a href='#' className="flex items-center px-2 py-2">
+                    <HandIcon className="h-5 w-5 text-gray-600" />
+                        <span>Service Store</span>
+                        </a>
                 </li>
                 <li>
-                    <a href='#'></a>
-                    <NewspaperIcon className="h-5 w-5 text-blue-500" />
-                    <span>News</span>
+                    <a href='#' className="flex items-center px-2 py-2">
+                    <NewspaperIcon className="h-5 w-5 text-gray-600" />
+                        <span>News</span>
+                        </a>
                 </li>
                 <li>
-                    <a href='#'></a>
-                    <CreditCardIcon className="h-5 w-5 text-blue-500" />
-                    <span>Finance</span>
+                    <a href='#' className="flex items-center px-2 py-2">
+                    <CreditCardIcon className="h-5 w-5 text-gray-600" />
+                        <span>Finance</span>
+                        </a>
                 </li>
                 <hr />
                 <li>
-                    <a href='#'></a>
-                    <QuestionMarkCircleIcon className="h-5 w-5 text-blue-500" />
-                    <span>Help</span>
+                    <a href='#' className="flex items-center px-2 py-2">
+                    <QuestionMarkCircleIcon className="h-5 w-5 text-gray-600" />
+                        <span>Help</span>
+                        </a>
                 </li>
             
             </ul>
